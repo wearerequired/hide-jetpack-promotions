ed5f62e5547c8644a6f1059ac481299c090d2622
Dominik Schilling <dominikschilling+git@gmail.com>
2017-07-04 13:44:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Jetpack without Promotions #
+
+* Contributors: wearerequired, ocean90
+* Tags: jetpack, promotions
+* Requires at least: 4.7
+* Tested up to: 4.7
+* Stable tag: 1.0.0
+* License: GPLv2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Removes all admin notices for promotions added by Jetpack.
+
+## Description ##
+
+This plugin helps you to declutter your WordPress admin by removing all Jetpack related notices that promote services like backup services or WordPress Apps. 
+
+## Changelog ##
+
+### 1.0.0 ###
+* Initial Release
---
+++ hide-jetpack-promotions.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Jetpack without Promotions
+ * Plugin URI:  https://github.com/wearerequired/hide-jetpack-promotions
+ * Description: Removes all admin notices for promotions added by Jetpack.
+ * Version:     1.0.0
+ * Author:      required
+ * Author URI:  https://required.com
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * Copyright (c) 2017 required (email: info@required.ch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package Required\HideJetpackPromotions
+ */
+
+add_filter( 'can_display_jetpack_manage_notice', '__return_false', 20 );
+add_filter( 'jetpack_just_in_time_msgs', '__return_false', 20 );
+add_filter( 'jetpack_show_promotions', '__return_false', 20 );
