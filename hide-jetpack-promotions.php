3a4771cb44376341e41def48dea62ae19a7ebf5d
Viktor Szépe <viktor@szepe.net>
2026-07-14 11:44:04 +0200

    Depend on Jetpack (#3)

--- hide-jetpack-promotions.php
+++ hide-jetpack-promotions.php
@@ -8,6 +8,7 @@
  * Author URI:  https://required.com
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Requires Plugins: jetpack
  *
  * Copyright (c) 2017-2023 required (email: info@required.ch)
  *
